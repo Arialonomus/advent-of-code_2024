14f8b1eea62a8ed387c555784fa6ba76769dc5af
Jacob Barber <barber.jacobervin@gmail.com>
2024-12-07 13:43:20 -0600

    Refactored for readability and consolidated code

--- day_02/red_nosed_reports-part1.py
+++ day_02/red_nosed_reports-part1.py
@@ -20,28 +20,27 @@ with args.input_file as file:
 # Iterate through each report to determine if it is safe
 total_safe_reports = 0
 for report in reports:
-    num_levels = len(report)
-    delta = report[0] - report[1]
-    is_increasing = delta < 0
 
-    def check_if_safe(difference):
+    def check_if_safe(difference, initial_difference):
         """
         Checks if the delta between two levels if safe.
-        Pre-Condition: Scoped variable is_increasing is set from the first 2 elements
-        in a report
         :param difference: The difference of two adjacent integer levels in a report
+        :param initial_difference: The difference of the first two integer levels in a report
         :return: True if the delta is safe, otherwise return False
         """
-        return ((difference < 0) == is_increasing and
-                SAFE_THRESHOLD_MIN <= abs(difference) <= SAFE_THRESHOLD_MAX)
+        directions_match = (difference < 0) == (initial_difference < 0)
+        within_safe_threshold = SAFE_THRESHOLD_MIN <= abs(difference) <= SAFE_THRESHOLD_MAX
+        return directions_match and within_safe_threshold
 
-    is_safe = check_if_safe(delta)
+    # Check safety of first two levels
+    initial_delta = report[0] - report[1]
+    is_safe = check_if_safe(initial_delta, initial_delta)
     i = 1
 
-    # Check the delta and direction of each level in series
+    # Check the delta and direction of each remaining level in report
+    num_levels = len(report)
     while i < num_levels - 1 and is_safe:
-        delta = report[i] - report[i + 1]
-        is_safe = check_if_safe(delta)
+        is_safe = check_if_safe(report[i] - report[i + 1], initial_delta)
         i += 1
 
     if is_safe:
